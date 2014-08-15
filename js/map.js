e20939269d52ad8b2313d0751a1dcfa5b66ef880
steven-king <cujophoto@gmail.com>
2014-08-15 10:50:23 -0400

    setup

---
+++ js/map.js
@@ -0,0 +1,44 @@
+  
+
+function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(generateMap, onError);
+        console.log(position); 
+    }
+
+
+function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
+    
+function generateMap(position) {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        var currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        
+        var mapOptions = {
+          //center: new google.maps.LatLng(-34.397, 150.644),
+          center: currentLatLng,
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+        
+         var marker = new google.maps.Marker({
+                position: currentLatLng,
+                map: map,
+                title: 'Current Location'
+  });
+      }
+
+
+/* For Browser testing. Comment out for app testing.
+ *
+ *var position ={
+        coords:{latitude:-34.397, longitude:150.644}
+}
+console.log(position.coords.latitude);
+
+
+google.maps.event.addDomListener(window, 'load',generateMap(position));
+*/
